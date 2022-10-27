ee41a1ab18d576caf8084e90833b1e878cefd331
imranfix <imran2000hosan@gmail.com>
2022-10-27 23:22:24 +0600

    added news api conditions and set data

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 
-const couuses = require('./data/courses.json');
-const courseInfo = require('./data/courseInfo.json');
+app.use(cors ());
+// data-1:
+const courses = require('./data/courses.json');
+// data-2:
+const news = require('./data/news.json');
 
 
 
@@ -13,13 +17,34 @@ app.get('/', (req, res)=>{
 });
 
 
-app.get('/programme-courses', (req, res) =>{
-    res.send(couuses);
+// data-1:
+app.get('/news-courses', (req, res) =>{
+    res.send(courses);
 })
 
 
-app.get('/programme-courseInfo', (req, res) =>{
-    res.send(courseInfo);
+app.get('/catagory/:id', (req, res) =>{
+    const id = req.params.id;
+    if(id === '07'){
+        res.send(news);
+    }
+    else{
+        const catagory_news = news.filter( n => n.catagory_id === id);
+        res.send(catagory_news);
+    }
+   
+})
+
+app.get('/news', (req, res) =>{
+    res.send(news);
+})
+
+
+// data:2 
+app.get('/news/:id', (req, res) =>{
+    const id = req.params.id;
+    const selected_news = news.find( n => n._id === id);
+    res.send(selected_news);
 })
 
 
